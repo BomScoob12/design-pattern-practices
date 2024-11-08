8cf516cb216fa50cfa1cf55a8f70537850796030
BomScoob12 <sorawitkraukham@gmail.com>
2024-11-08 16:12:16 +0700

    feat: prototype design pattern.

---
+++ src/main/java/com/bomscoob/base/prototype/Monster.java
@@ -0,0 +1,27 @@
+package com.bomscoob.base.prototype;
+
+public class Monster implements Prototype {
+    private String name;
+
+    public Monster(String name) {
+        this.name = name;
+    }
+
+    public Monster(Monster monster) {
+        this.name = monster.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    // can not use clone because it override the Objects.clone()
+    @Override
+    public Monster cloneMonster() {
+        return new Monster(this.name);
+    }
+}
